3accac98fa5aa8747b58f61e3c39e1ced913f011
Daniel Orija <danielorijadribo@gmail.com>
2023-11-29 13:23:25 +0300

    File upload name change

--- src/components/Upload.js
+++ src/components/Upload.js
@@ -4,9 +4,19 @@ import { ToastContainer, toast } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
 
 const Upload = () => {
+  const [selectedFile, setSelectedFile] = useState();
+  const [isFilePicked, setIsFilePicked] = useState(false);
+
+  const changeHandler = (event) => {
+    setSelectedFile(event.target.files[0]);
+    setIsFilePicked(true);
+  }
+
+
+
   const fetchDatabaseData = async () => {
     const db = getDatabase();
-    const collectionRef = ref(db, "bankDetails"); // Replace with your actual collection name
+    const collectionRef = ref(db, "bankDetails"); 
     const snapshot = await get(collectionRef);
 
     if (snapshot.exists()) {
@@ -43,8 +53,6 @@ const Upload = () => {
 
       if (matchingRecord) {
         console.log("Details match an existing record in the database.");
-        // setSubmission(true);
-        // setUnverified(true);
 
         //Floating message to the user upon successfull verification
         toast.success("Verification Successfull!", {
@@ -54,8 +62,6 @@ const Upload = () => {
         console.log(
           "Details do not match any existing records in the database."
         );
-        // setSubmission(true);
-        // setUnverified(false);
 
         //Floating message to the user upon unsuccessfull verification
         toast.error("Verification Unsuccessfull!", {
@@ -64,60 +70,17 @@ const Upload = () => {
       }
     }
 
-    // Reset isVisible to true after each submission
-    // setIsVisible(true);
-
     handleClearForm();
   };
 
   const handleClearForm = () => {
     // Clear all form fields
     document.getElementById("uploadForm").reset();
+    setSelectedFile();
   };
 
-  // const [verified, setUnverified] = useState(false);
-  // const [submission, setSubmission] = useState(false);
-
-  // const [isVisible, setIsVisible] = useState(true);
-
-  // useEffect(() => {
-  //   const timeoutId = setTimeout(() => {
-  //     setIsVisible(false);
-  //   }, 5000);
-
-  //   // Clear the timeout to avoid side effects on component unmount or state changes
-  //   return () => clearTimeout(timeoutId);
-  // }, [isVisible]);
-
   return (
-    <div id="upload" className={`px-10 py-12 md:py-5 w-full `}>
-      {/* Display message upon successfull or unsuccessful verification */}
-
-      {/* <div>
-        {isVisible && (
-          <div
-            className={`sticky z-50 top-[200px] right-0 text-center w-[100%] ${
-              submission ? "" : "hidden"
-            }`}
-          >
-            <div
-              className={`bg-green-200 rounded-lg p-2 ${
-                verified ? "" : "hidden"
-              }`}
-            >
-              Verification successfull
-            </div>
-            <div
-              className={`bg-red-200 rounded-lg p-2 ${
-                verified ? "hidden" : ""
-              }`}
-            >
-              Verification unsuccessfull
-            </div>
-          </div>
-        )}
-      </div> */}
-
+    <div id="upload" className="px-10 py-12 md:py-5 w-full">
       <h1 className="font-bold text-2xl">Upload Form</h1>
       <div className="py-10">
         {/* Update the form with action and submit button */}
@@ -181,35 +144,57 @@ const Upload = () => {
               htmlFor="dropzone-file"
               className="flex flex-col items-center justify-center w-full h-64 border-2 border-gray-300 border-dashed rounded-lg cursor-pointer bg-[#02B056]/[0.1] hover:bg-[#02B056]/[0.3]"
             >
-              <div className="flex flex-col items-center justify-center pt-5 pb-6">
-                <svg
-                  className="w-8 h-8 mb-4 text-gray-500 dark:text-gray-400"
-                  aria-hidden="true"
-                  xmlns="http://www.w3.org/2000/svg"
-                  fill="none"
-                  viewBox="0 0 20 16"
-                >
-                  <path
-                    stroke="currentColor"
-                    strokeLinecap="round"
-                    strokeLinejoin="round"
-                    strokeWidth="2"
-                    d="M13 13h3a3 3 0 0 0 0-6h-.025A5.56 5.56 0 0 0 16 6.5 5.5 5.5 0 0 0 5.207 5.021C5.137 5.017 5.071 5 5 5a4 4 0 0 0 0 8h2.167M10 15V6m0 0L8 8m2-2 2 2"
-                  />
-                </svg>
-                <p className="mb-2 text-sm text-gray-500 dark:text-gray-400">
-                  <span className="font-semibold">Click to upload</span> or drag
-                  and drop
-                </p>
-                <p className="text-xs text-gray-500 dark:text-gray-400">
-                  Image or PDF
-                </p>
-              </div>
+
+              {/* Place holder for file input and change name according to file input */}
+              {selectedFile ? (
+                <div>
+                  <p className="mb-2 text-md text-gray-500 dark:text-gray-400">
+                    <span className="font-bold">Filename: </span>
+                    {selectedFile.name}
+                  </p>
+                  <p className="mb-2 text-md text-gray-500 dark:text-gray-400">
+                    <span className="font-bold">Filetype: </span>
+                    {selectedFile.type}
+                  </p>
+                  <p className="mb-2 text-md text-gray-500 dark:text-gray-400">
+                    <span className="font-bold">Size in bytes: </span>
+                    {(selectedFile.size / 1024).toFixed(2) + " KB"}
+                  </p>
+                </div>
+              ) : (
+                <div className="flex flex-col items-center justify-center pt-5 pb-6">
+                  <svg
+                    className="w-8 h-8 mb-4 text-gray-500 dark:text-gray-400"
+                    aria-hidden="true"
+                    xmlns="http://www.w3.org/2000/svg"
+                    fill="none"
+                    viewBox="0 0 20 16"
+                  >
+                    <path
+                      stroke="currentColor"
+                      strokeLinecap="round"
+                      strokeLinejoin="round"
+                      strokeWidth="2"
+                      d="M13 13h3a3 3 0 0 0 0-6h-.025A5.56 5.56 0 0 0 16 6.5 5.5 5.5 0 0 0 5.207 5.021C5.137 5.017 5.071 5 5 5a4 4 0 0 0 0 8h2.167M10 15V6m0 0L8 8m2-2 2 2"
+                    />
+                  </svg>
+                  <p className="mb-2 text-sm text-gray-500 dark:text-gray-400">
+                    <span className="font-semibold">Click to upload</span> or
+                    drag and drop
+                  </p>
+                  <p className="text-xs text-gray-500 dark:text-gray-400">
+                    Image or PDF
+                  </p>
+                </div>
+              )}
+
               <input
                 id="dropzone-file"
                 type="file"
                 className="hidden"
                 required
+                onChange={changeHandler}
+                accept=".pdf, image/*"
               />
             </label>
           </div>
