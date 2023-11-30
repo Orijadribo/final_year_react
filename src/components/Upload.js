20eae097af102450effd7e14149fa5c32cdb6912
Daniel Orija <danielorijadribo@gmail.com>
2023-11-30 13:44:40 +0300

    new

--- src/components/Upload.js
+++ src/components/Upload.js
@@ -1,115 +1,87 @@
-import React, { useState } from "react";
-import { getDatabase, get, ref, push, set } from "firebase/database";
+import React, { useState, useEffect } from "react";
+import { getDatabase, ref, get } from "firebase/database";
 import { ToastContainer, toast } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
 
 const Upload = () => {
-  const [error, setError] = useState("");
+  const [selectedFile, setSelectedFile] = useState();
+  const [isFilePicked, setIsFilePicked] = useState(false);
 
-  const sendDataToDatabase = async (data, listName) => {
+  const changeHandler = (event) => {
+    setSelectedFile(event.target.files[0]);
+    setIsFilePicked(true);
+  }
+
+  const fetchDatabaseData = async () => {
     const db = getDatabase();
-    const listRef = ref(db, listName);
-  
-    try {
-      const { payer, recordKey, ...details } = data;
-  
-      // Fetch existing data from the database
-      const existingData = await fetchDatabaseData(listName);
-  
-      // Create or update the payer's data
-      const payerData = {
-        ...existingData[payer],
-        [recordKey]: {
-          amount: details.amount,
-          date: details.date,
-          payer: details.payer,
-          regNo: details.regNo,
-        },
-      };
-  
-      // Set the data in the database
-      await set(ref(listRef, payer), payerData);
-  
-      console.log(`Data successfully added to ${listName}. Payer: ${payer}, Record Key: ${recordKey}`);
-    } catch (error) {
-      console.error(`Error adding data to ${listName}:`, error.message);
+    const collectionRef = ref(db, "bankDetails"); 
+    const snapshot = await get(collectionRef);
+
+    if (snapshot.exists()) {
+      // Extract data from the snapshot
+      const databaseData = snapshot.val();
+      return databaseData;
+    } else {
+      console.log("No data found in the collection.");
+      return null;
     }
   };
-  
-
-
-  const fetchDatabaseData = async (listName) => {
-    const db = getDatabase();
-    const listRef = ref(db, listName);
-
-    try {
-      const snapshot = await get(listRef);
-      if (snapshot.exists()) {
-        return snapshot.val();
-      } else {
-        console.log(`No data available in ${listName}`);
-        return {};
-      }
-    } catch (error) {
-      console.error(`Error fetching data from the database (${listName}):`, error.message);
-      return {};
-    }
-  };
-
 
   const handleFormSubmission = async (event) => {
     event.preventDefault();
-  
-    const amount = Number(document.getElementById("amount").value);
+
+    // Get form data
+    const amount = parseFloat(document.getElementById("amount").value);
     const date = document.getElementById("date").value;
     const regNo = document.getElementById("regNo").value;
     const payer = document.getElementById("payer").value;
-  
-    const bankDetails = await fetchDatabaseData("bankDetails");
-    const deniedList = await fetchDatabaseData("deniedList");
-    const verifiedList = await fetchDatabaseData("verifiedList");
-  
-    const successMessage = "Verification Successful!";
-    const errorMessage = "Verification Unsuccessful!";
-  
-    if (bankDetails && deniedList && verifiedList) {
-      const matchingRecordBank = Object.values(bankDetails).find(
+
+    // Fetch data from the database
+    const databaseData = await fetchDatabaseData();
+
+    // Compare form data with database data
+    if (databaseData) {
+      const matchingRecord = Object.values(databaseData).find(
         (record) =>
           record.amount === amount &&
           record.date === date &&
           record.regNo === regNo &&
           record.payer === payer
       );
-  
-      if (matchingRecordBank) {
-        console.log("Details match an existing record in the bank details.");
-        const listName = "verifiedList";
-        sendDataToDatabase({ amount, date, regNo, payer }, listName);
-        toast.success(successMessage, {
+
+      if (matchingRecord) {
+        console.log("Details match an existing record in the database.");
+
+        //Floating message to the user upon successfull verification
+        toast.success("Verification Successfull!", {
           position: toast.POSITION.TOP_CENTER,
         });
       } else {
-        console.log("Details do not match any existing records in the bank details.");
-        const listName = "deniedList";
-        sendDataToDatabase({ amount, date, regNo, payer }, listName);
-        toast.error(errorMessage, {
+        console.log(
+          "Details do not match any existing records in the database."
+        );
+
+        //Floating message to the user upon unsuccessfull verification
+        toast.error("Verification Unsuccessfull!", {
           position: toast.POSITION.TOP_CENTER,
         });
       }
     }
-  
+
     handleClearForm();
   };
-  
-  
 
   const handleClearForm = () => {
+    // Clear all form fields
     document.getElementById("uploadForm").reset();
+    setSelectedFile();
   };
 
   return (
-    <div id="upload" className={`px-10 py-12 md:py-5 w-full `}>
+    <div id="upload" className="px-10 py-12 md:py-5 w-full">
       <h1 className="font-bold text-2xl">Upload Form</h1>
       <div className="py-10">
+        {/* Update the form with action and submit button */}
         <form
           action=""
           className="flex flex-col"
@@ -170,35 +142,57 @@ const Upload = () => {
               htmlFor="dropzone-file"
               className="flex flex-col items-center justify-center w-full h-64 border-2 border-gray-300 border-dashed rounded-lg cursor-pointer bg-[#02B056]/[0.1] hover:bg-[#02B056]/[0.3]"
             >
-              <div className="flex flex-col items-center justify-center pt-5 pb-6">
-                <svg
-                  className="w-8 h-8 mb-4 text-gray-500 dark:text-gray-400"
-                  aria-hidden="true"
-                  xmlns="http://www.w3.org/2000/svg"
-                  fill="none"
-                  viewBox="0 0 20 16"
-                >
-                  <path
-                    stroke="currentColor"
-                    strokeLinecap="round"
-                    strokeLinejoin="round"
-                    strokeWidth="2"
-                    d="M13 13h3a3 3 0 0 0 0-6h-.025A5.56 5.56 0 0 0 16 6.5 5.5 5.5 0 0 0 5.207 5.021C5.137 5.017 5.071 5 5 5a4 4 0 0 0 0 8h2.167M10 15V6m0 0L8 8m2-2 2 2"
-                  />
-                </svg>
-                <p className="mb-2 text-sm text-gray-500 dark:text-gray-400">
-                  <span className="font-semibold">Click to upload</span> or drag
-                  and drop
-                </p>
-                <p className="text-xs text-gray-500 dark:text-gray-400">
-                  Image or PDF
-                </p>
-              </div>
+
+              {/* Place holder for file input and change name according to file input */}
+              {selectedFile ? (
+                <div>
+                  <p className="mb-2 text-md text-gray-500 dark:text-gray-400">
+                    <span className="font-bold">Filename: </span>
+                    {selectedFile.name}
+                  </p>
+                  <p className="mb-2 text-md text-gray-500 dark:text-gray-400">
+                    <span className="font-bold">Filetype: </span>
+                    {selectedFile.type}
+                  </p>
+                  <p className="mb-2 text-md text-gray-500 dark:text-gray-400">
+                    <span className="font-bold">Size in bytes: </span>
+                    {(selectedFile.size / 1024).toFixed(2) + " KB"}
+                  </p>
+                </div>
+              ) : (
+                <div className="flex flex-col items-center justify-center pt-5 pb-6">
+                  <svg
+                    className="w-8 h-8 mb-4 text-gray-500 dark:text-gray-400"
+                    aria-hidden="true"
+                    xmlns="http://www.w3.org/2000/svg"
+                    fill="none"
+                    viewBox="0 0 20 16"
+                  >
+                    <path
+                      stroke="currentColor"
+                      strokeLinecap="round"
+                      strokeLinejoin="round"
+                      strokeWidth="2"
+                      d="M13 13h3a3 3 0 0 0 0-6h-.025A5.56 5.56 0 0 0 16 6.5 5.5 5.5 0 0 0 5.207 5.021C5.137 5.017 5.071 5 5 5a4 4 0 0 0 0 8h2.167M10 15V6m0 0L8 8m2-2 2 2"
+                    />
+                  </svg>
+                  <p className="mb-2 text-sm text-gray-500 dark:text-gray-400">
+                    <span className="font-semibold">Click to upload</span> or
+                    drag and drop
+                  </p>
+                  <p className="text-xs text-gray-500 dark:text-gray-400">
+                    Image or PDF
+                  </p>
+                </div>
+              )}
+
               <input
                 id="dropzone-file"
                 type="file"
                 className="hidden"
                 required
+                onChange={changeHandler}
+                accept=".pdf, image/*"
               />
             </label>
           </div>
